28ba153a7d9061bf81a35ebcf7c38a9fa1e0878f
Joa <86939873+joa-jo@users.noreply.github.com>
2022-05-26 13:55:08 +0900

    :note: emit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,73 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="container">
+  <h1 class="todo-title">투두리스트</h1>
+  <TodoForm @add-todo="addTodo"/>
+  <strong v-if="!todos.length">You have nothing to do.</strong>
+
+  <ul v-for="(t, index) in todos" :key="t.id" class="card mt-2">
+    <li  class="card-body d-flex justify-space-between p-2 flex-grow-1">
+      <label :for="t.id" class="card-text" :class="{doneTodo: t.completed}">
+        <input type="checkbox" class="form-input-check" :id="t.id" v-model="t.completed">
+        {{t.subject}}
+      </label>
+      <div>
+        <button class="btn btn-danger btn-sm" @click="deleteTodo(index)">Delete</button>
+      </div>
+    </li>
+  </ul>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {ref} from 'vue';
+import TodoForm from './components/TodoForm.vue'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
+    TodoForm
+  },
+  setup() {
+      const todos = ref([]);
+
+      const addTodo = (todo) => {
+        todos.value.push(todo)
+      }
+
+      const deleteTodo = (index) => {
+          todos.value.splice(index, 1);
+      };
+      return {
+          todos,
+          addTodo,
+          deleteTodo,
+      };
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+* {
+  list-style: none;
+}
+
+.container {
+  margin-top: 100px;
+}
+
+.todo-title {
+  margin-bottom: 50px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-size: 50px;
+  font-weight: 600;
+  color: rgb(66, 66, 221);
+}
+
+.card-text {
+  cursor: pointer;
+}
+
+.doneTodo {
+  color: lightgrey;
+  text-decoration: line-through;
 }
 </style>
---
+++ src/components/TodoForm.vue
@@ -0,0 +1,47 @@
+<template>
+<form action="" class="d-flex" @submit.prevent="onSubmit">
+    <div class="flex-grow-1 mr-2">
+      <input type="text" class="form-control" v-model="todo" placeholder="Any plan for today?">
+    </div>
+    <div>
+      <button class="btn btn-primary" type="submit">
+        Add
+      </button>
+    </div>
+  </form>
+
+</template>
+
+<script>
+import {ref} from 'vue';
+
+export default {
+    setup(props, context) {
+        const hasError = ref(false);
+        const todo = ref("");
+
+        const onSubmit = () => {
+            if(todo.value === '') {
+                hasError.value = true; 
+            } else {
+                context.emit('add-todo', {
+                    id: Date.now(),
+                    subject: todo.value,
+                    completed: false,
+                })
+                hasError.value = false;
+                todo.value = "";
+            }
+        };
+
+        return {
+            todo,
+            hasError,
+            onSubmit,
+        }
+    }
+
+}
+</script>
+
+<style></style>
